84922ca71a5b741ba2b64cbd0e8dfdbdd4b1eaf4
phantom05 <monster2jy@gmail.com>
2019-06-20 15:04:26 +0900

    TESTING

---
+++ server.js
@@ -0,0 +1,86 @@
+
+
+
+// var WebSocketServer = require("ws").Server;
+// var wss = new WebSocketServer({ port: 3000 });
+
+// 연결이 수립되면 클라이언트에 메시지를 전송하고 클라이언트로부터의 메시지를 수신한다
+// wss.on("connection", function(ws) {
+//   ws.send("Hello! I am a server.");
+//   ws.on("message", function(message) {
+//     console.log("Received: %s", message);
+//   });
+// });
+
+var app = require('express')();
+var server = require('http').createServer(app);
+// http server를 socket.io server로 upgrade한다
+var io = require('socket.io')(server);
+
+// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.htm');
+  // res.render('index')
+});
+
+app.get('/hello', function(req, res) {
+  res.sendFile(__dirname + '/hello.htm');
+  // res.render('index')
+});
+
+// connection event handler
+// connection이 수립되면 event handler function의 인자로 socket인 들어온다
+io.on('connection', function(socket) {
+
+  // 접속한 클라이언트의 정보가 수신되면
+  socket.on('login', function(data) {
+    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = data.name;
+    socket.userid = data.userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('login', data.name );
+  });
+
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', function(data) {
+    // console.log(socket,data);
+    console.log('Message from %s: %s', socket.name, data.msg);
+
+    var msg = {
+      from: {
+        name: socket.name,
+        userid: socket.userid
+      },
+      msg: data.msg
+    };
+
+    console.log(msg);
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit('chat', msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    // io.emit('s2c chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
+  });
+
+  // force client disconnect from server
+  socket.on('forceDisconnect', function() {
+    socket.disconnect();
+  })
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected: ' + socket.name);
+  });
+});
+
+server.listen(3000, function() {
+  console.log('Socket IO server listening on port 3000');
+});
